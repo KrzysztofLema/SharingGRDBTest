cf499cd09c0a2a6eecec83f20a3769186a42d225
Krzysztof Lema <lema10@o2.pl>
2025-07-20 11:02:38 +0200

    Initial Commit

---
+++ SharingGRDBTest/SharingGRDBTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharingGRDBTestApp.swift
+//  SharingGRDBTest
+//
+//  Created by Krzysztof Lema on 20/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct SharingGRDBTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SharingGRDBTestTests/SharingGRDBTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  SharingGRDBTestTests.swift
+//  SharingGRDBTestTests
+//
+//  Created by Krzysztof Lema on 20/07/2025.
+//
+
+import Testing
+@testable import SharingGRDBTest
+
+struct SharingGRDBTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
